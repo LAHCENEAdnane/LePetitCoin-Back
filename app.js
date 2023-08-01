65d1d519bf125dfffcc24f05265d7873d9be536b
Philip Traas <phtraas@gmail.com>
2023-08-01 20:05:19 +0200

    added toilet route in backend

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
+let toiletsRouter = require('./routes/toilets')
 const cors = require('cors');
 
 
@@ -22,5 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/toilets', toiletsRouter);
 
 module.exports = app;
