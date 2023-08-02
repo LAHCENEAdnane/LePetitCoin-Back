6b831ad14d13341cfc1e751108bf73693ee11f09
Adnane <adnane.lahcene@icloud.com>
2023-08-02 18:31:59 +0200

    route review incomplete

--- routes/review.js
+++ routes/review.js
@@ -1,27 +1,40 @@
 let express = require('express');
 let router = express.Router();
 const Review = require('../models/Review');
+const Toilet = require ('../models/Toilet')
+const User = require('../models/Users')
 
-router.post('/', async (req, res) => {
-  try {
-    const { review, rating, title, users, toilet } = req.body; // Récupérer les données du corps de la requête
+router.post('/', (req, res) => {
+  const { review, rating, title, userToken, toiletId } = req.body;
 
-    // Créer une nouvelle instance de Review avec les données reçues
-    const newReview = new Review({
-      review: review,
-      rating: rating,
-      title: title,
-      users: users,
-      toilet: toilet
-    });
+  User.findOne({ token: userToken }).then(data => {
+    if (data) {
+      const newReview = new Review({
+        review: review,
+        rating: rating,
+        title: title,
+        users: data._id,
+        toilet: toiletId
+      });
 
-    const savedReview = await newReview.save(); // Enregistrement de la nouvelle instance de Review
-    res.json(savedReview); // Renvoyer la réponse avec la nouvelle instance de Review créée
-  } catch (error) {
-    // En cas d'erreur lors de l'enregistrement de la Review
+      newReview.save().then(data => {
+        // Renvoyer la réponse avec la nouvelle instance de Review créée
+        const reviewId = data._id;
+        console.log(reviewId);
+        // res.json(data);
+      }).catch(error => {
+        // En cas d'erreur lors de l'enregistrement de la Review
+        console.error(error);
+        res.status(500).json({ error: 'Une erreur est survenue lors de l\'enregistrement de la review.' });
+      });
+    } else {
+      res.status(404).json({ error: 'Utilisateur non trouvé.' });
+    }
+  }).catch(error => {
     console.error(error);
-    res.status(500).json({ error: 'Une erreur est survenue lors de l\'enregistrement de la review.' });
-  }
+    res.status(500).json({ error: 'Une erreur est survenue lors de la recherche de l\'utilisateur.' });
+  });
 });
+
 
 module.exports = router;
