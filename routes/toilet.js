099184bc3aa189ca7eec94f8473237acf3547754
Adnane <adnane.lahcene@icloud.com>
2023-08-02 08:37:18 +0200

    route post and get

--- routes/toilet.js
+++ routes/toilet.js
@@ -4,13 +4,13 @@ const Toilet = require('../models/Toilet');
 
 router.post('/', async (req, res) => {
   try {
-    const {address, pictures, type, avaibality, handicapAccess, coatHanger, changingTable, soap,toiletPaper,cleanliness, feminineHygieneProduct, longitude, latitude} = req.body
+    const {address, pictures, type, availability, handicapAccess, coatHanger, changingTable, soap,toiletPaper,cleanliness, feminineHygieneProduct, longitude, latitude} = req.body
     // Créer une nouvelle instance de Review avec les données reçues
     const newToilet = new Toilet({
         address,
         // pictures,
         type, 
-        avaibality, 
+        availability, 
         handicapAccess, 
         coatHanger, 
         changingTable, 
@@ -30,5 +30,15 @@ router.post('/', async (req, res) => {
     res.status(500).json({ error: 'Une erreur est survenue lors de l\'enregistrement de la Toilet.' });
   }
 });
+
+
+
+router.get('/:toilet',(req,res) => {
+    const toiletId = req.params.id
+    Toilet.findOne({ id: toiletId })
+ .then(data => {
+   console.log(data);
+ });
+})
 
 module.exports = router;
