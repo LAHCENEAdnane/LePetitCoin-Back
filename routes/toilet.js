894e26f3c2119b7bf14933cbe3e7049ec9c87748
Adnane <adnane.lahcene@icloud.com>
2023-08-03 15:37:17 +0200

    test limit bd

--- routes/toilet.js
+++ routes/toilet.js
@@ -56,7 +56,7 @@ router.get("/", (req, res) => {
 //     res.json({ result: false, error: "taper une ville" });
 //     return;
 //   }
-  Toilet.find({ commune: { $regex: new RegExp(req.body.commune, "i") } }).then(
+  Toilet.find({ commune: { $regex: new RegExp(req.body.commune, "i") } }).limit(20).then(
     (data) => {
       if (data === null) {
         res.json({ result: false, error: "mince, c'est schrodingers coin" });
