7412a4e40b5b4636d36dadd77f2ecd7570f87072
Manon ERB <manon.erb@gmail.com>
2023-08-06 18:12:31 +0200

    ajout fun facts, test

--- models/FunFacts.js
+++ models/FunFacts.js
@@ -6,6 +6,7 @@ var FunFactsSchema = new Schema({
   },
   title: {
     type: String
+    
   },
   text: {
     type: String
--- routes/funFacts.js
+++ routes/funFacts.js
@@ -1,81 +1,28 @@
 let express = require('express');
 let router = express.Router();
-const FunFacts = require('../models/FunFacts')
+const FunFact = require('../models/FunFacts');
 
-router.post('/:token', (req, res) => {
-
-  const {token} = req.params
-  const { review, rating, title, toiletId } = req.body;
-
-  User.findOne({ token }).then(data => {
-    console.log(data)
-    if (data) {
-      const newReview = new Review({
-        title: title,
-        rating: rating,
-        review: review,
-        user: data._id,
-        toilet: toiletId
-      });
-
-      newReview.save().then(data => {
-        // Renvoyer la réponse avec la nouvelle instance de Review créée
-        const reviewId = data._id;
-        console.log(reviewId);
-        // res.json(data);
-      })
-      .catch(error => {
-        // En cas d'erreur lors de l'enregistrement de la Review
-        console.error(error);
-        res.status(500).json({ error: 'Une erreur est survenue lors de l\'enregistrement de la review.' });
-      });
-    } else {
-      res.status(404).json({ error: 'Utilisateur non trouvé.' });
+//   router.get('/', (req, res) => {
+// // pourquoi .params déjà ? pour récupérer ce qu'il y aura dans l'url
+//     FunFact.findOne( { title : req.body.title, text : req.body.text } )
+//     .then(data => {
+//       console.log(data);
+//       res.json({data});
+//     });
+//   });
+  router.get('/funfact', async (req, res) => {
+    try {
+        //await est utilisé pour attendre que la requête MongoDB soit terminée avant de continuer, s'utilise avec async
+      const funFact = await FunFact.findOne().skip(Math.floor(Math.random() * await FunFact.countDocuments()));
+      //.countDocuments, pas obligatoire, exécute une requête MongoDB pour compter le nb total de docs dans la collection FunFact.
+     // skip permet de sauter le numéro aléatoire précedemment sorti
+      res.json(funFact);
+    } catch (err) {
+      res.status(500).json({ message: 'Erreur lors de la récupération du fun fact.' });
     }
-  }).catch(error => {
-    console.error(error);
-    res.status(500).json({ error: 'Une erreur est survenue lors de la recherche de l\'utilisateur.' });
   });
-});
 
-
-router.put('/:token',(req,res) => {
-
-  const {token} = req.params;
-  const { review, rating, title, toiletId } = req.body;
   
-  User.findOne({ token }).then(data => {
-    if (data) {
-      // Vérifiez que l'utilisateur a bien créé l'avis que vous essayez de mettre à jour
-      Review.findOne({ user: data._id }).then(data => {
-        if (data) {
-          // Mettez à jour les champs de l'avis
-          data.title = title;
-          data.rating = rating;
-          data.review = review;
-
-          // Enregistrez les modifications dans la base de données
-          data.save().then(updatedReview => {
-            res.json(updatedReview);
-          }).catch(error => {
-            console.error(error);
-            res.status(500).json({ error: 'Une erreur est survenue lors de la mise à jour de l\'avis.' });
-          });
-        } else {
-          res.status(404).json({ error: 'Avis non trouvé ou vous n\'avez pas l\'autorisation de le modifier.' });
-        }
-      }).catch(error => {
-        console.error(error);
-        res.status(500).json({ error: 'Une erreur est survenue lors de la recherche de l\'avis.' });
-      });
-    } else {
-      res.status(404).json({ error: 'Utilisateur non trouvé.' });
-    }
-  }).catch(error => {
-    console.error(error);
-    res.status(500).json({ error: 'Une erreur est survenue lors de la recherche de l\'utilisateur.' });
-  });
-})
-
+   //FunFact = nom du modèle
 
 module.exports = router;
